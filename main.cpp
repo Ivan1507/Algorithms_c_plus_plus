b9e335cec39d1d48c70f5806fa1dcbc8777c7468
Ivan1507 <iancurtis1507@mail.ru>
2021-01-08 16:24:41 +0300

    Динамическое программирование: Кузнечик - художник

--- main.cpp
+++ main.cpp
@@ -1,41 +1,28 @@
 #include <iostream>
-#include <stack>
-#include <math.h>
-#include <map>
 #include <vector>
+#include <string>
 using namespace std;
 
-int main(){
-    int n;
-    cin>>n;
-    int A[n];
-    int B[n];
-    bool truth[n];
-    for(int i=0;i<n;i++){
-            truth[i]=false;
-        cin>>A[i]>>B[i];
-    }
-    int ans=0;
-    for(int i=0;i<n;i++){
-         if(A[i]>=n||B[i]>=n) ans+=1;
-         else{
-            if(!B[i]){
-                    if(truth[n-A[i]-1])
-                ans+=1;
-                else truth[n-A[i]-1]=true;
-            }
-            else if(!A[i]){
-                if(truth[B[i]])
-                    ans+=1;
-                else truth[B[i]]=true;
-            }
-            else{
-                 int k=n-A[i]-1;
-                 if(B[i]!=k) ans+=1;
-                else if(truth[k]) ans+=1;
-                   else truth[k]=true;
-            }
-         }
-    }
-    cout<<n-ans;
+int main()
+{
+int n;
+cin>>n;
+int C[n];
+for(int i=0;i<n;i++)
+    cin>>C[i];
+int K[n];
+for(int i=0;i<n;i++)
+    cin>>K[i];
+int *A=new int[n];
+A[0]=1;
+if(C[1]==C[0]||C[1]==K[0]) A[1]=1;
+else A[1]=0;
+for(int i=2;i<n;i++){
+     if((C[i]==C[i-2]||C[i]==K[i-2])&&(C[i]==C[i-1]||C[i]==K[i-1])) A[i]=(A[i-1]+A[i-2])%947;
+     else if(C[i]==C[i-2]||C[i]==K[i-2]) A[i]=A[i-2]%947;
+     else if(C[i]==C[i-1]||C[i]==K[i-1]) A[i]=A[i-1]%947;
+     else A[i]=0;
+}
+cout<<A[n-1];
+delete[] A;
 }
